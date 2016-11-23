e1ba69f2ebcbaef8b714bf179216d8b42679109a
= <=>
2016-11-23 23:30:19 +0100

    Finishing the app

--- js/app.js
+++ js/app.js
@@ -16,41 +16,81 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	// Planification
 	// =============
 
-	// Selectionner tous les élèves de la page
-	var studentItem = document.getElementsByClassName("student-item");
-	var studentList = document.getElementsByClassName("student-list");
+	/**
+	 * Store the main body of the page
+	 * @type {node}
+	 */
 	var page = document.getElementsByClassName("page")[0];
 
+	/**
+	 * Store the students and the student list
+	 * @type {node}
+	 */
+	var studentItem = document.getElementsByClassName("student-item");
+	/**
+	 * Create a pagination button for each 10 students in the page
+	 */
 	var createButton = function () {
-		var pagination	= document.createElement("div")
-		var ul			= document.createElement("ul");
+
+		/**
+		 * Create and store the elements of buttons
+		 */
+		var pagination = document.createElement("div")
+		var ul = document.createElement("ul");
 
 		pagination.className = "pagination";
 
 		page.appendChild(pagination);
 		pagination.appendChild(ul);
 
-		var paginationCounter = 0;
+		var paginationNumber = 0;
 
 		for (i = 0; i <= studentItem.length; i = i + 10) {
-		// 	Procurer une pagination pour chaque 10 élèves dans la page
+
 			var li 	= document.createElement("li");
 			var a 	= document.createElement("a");
 			
 			ul.appendChild(li);
 			li.appendChild(a);
 
-			paginationCounter = paginationCounter + 1
+			paginationNumber = paginationNumber + 1
 
-			a.innerText = paginationCounter;
+			a.setAttribute("href", "#");
+			a.innerText = paginationNumber;
 		}
 	}
 
-	var displayStudents = function (studentList) {
-		// Quand je clique sur un bouton de la pagination
+	var createPlaceHolder = function () {
+		var pageHeader = document.getElementsByClassName("page-header")[0];
+
+		var studentSearch = document.createElement("div");
+		studentSearch.className = "student-search";
+
+		var input = document.createElement("input");
+		input.setAttribute("placeholder", "Search for students...");
+
+		var searchButton = document.createElement("button");
+		searchButton.innerText = "Search";
+
+		pageHeader.appendChild(studentSearch);
+		studentSearch.appendChild(input);
+		studentSearch.appendChild(searchButton);		
+	}
+
+	var displayStudents = function () {
+
+		for (var i = 0; i < studentItem.length; i++) {
+			studentItem[i].style.display = "none";
+		}
+
+		for (var i = 0; i <= 10; i++) {
+			studentItem[i].style.display = "block";
+		}
+
 		var buttons = document.querySelectorAll(".pagination a");
 		for (var i = 0; i < buttons.length; i++) {
 			buttons[i].addEventListener("click", function () {
+				console.log("click");
 				var linkNumber = this.innerText;
 				var studentToHide = document.querySelectorAll(".student-item");
 				for (var i = 0; i < studentToHide.length; i++) {
@@ -63,8 +103,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				}
 			})
 		}
-	}																																																																																																				 																													
+	}
 
+	var searchStudent = function () {
+		var searchInput = document.querySelector(".student-search input");
+		var searchButton = document.querySelector(".student-search button");
+		var studentName = document.querySelectorAll(".student-item h3");
+		searchButton.addEventListener("click", function (st) {
+
+			var str = searchInput.value;
+			console.log(str);
+			for (var i = 0; i < studentItem.length; i++) {
+				studentItem[i].style.display = "none";
+			}
+			for (var i = 0; i < studentName.length; i++) {
+				var name = studentName[i].innerText;
+				var reg = new RegExp(str, "gi");
+				if (name.match(reg)) {
+					studentItem[i].style.display = "block";
+				}
+			}
+
+		})
+
+		console.log(searchInput);
+	}
+	
 	createButton();
-	displayStudents(studentList);
+	createPlaceHolder();
+	displayStudents();
+	searchStudent();
 });
